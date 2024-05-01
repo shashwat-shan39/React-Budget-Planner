3df38a4758767fc28d6737b9c2ed8d6d328320e9
shashwat <sshandilya1833@gmail.com>
2024-05-01 15:02:38 +0530

    log

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1>hello</h1>
+    {console.log("hello")}
    <App />
   </React.StrictMode>
 );
